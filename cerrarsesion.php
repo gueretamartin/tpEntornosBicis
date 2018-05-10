c31625be7ccb7d7d31a8a03e3d9ff5c3f299b15a
alvfacu <alvfacu@gmail.com>
2018-05-10 19:08:23 -0300

    Elimino archivos duplicados

--- cerrarsesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['usuario']);
-
-setcookie("recordar", "", 0);
-
-header("location:index.php");
-?>
--- pie.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '
-<div id="footer">
-  <div class="container">
-    <p class="text-center credit">Reserva de Bicicletas, Rodriguez 86, Rosario, Santa Fe, Argentina, 2018.</p>
-  </div>
-</div>'
-
-?>
