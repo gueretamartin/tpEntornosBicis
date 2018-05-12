38f90a8cd55e2cec8957340961caeae59e7a53e9
alvfacu <alvfacu@gmail.com>
2018-05-12 16:06:52 -0300

    Version inicial de la db

--- biciamiga.sql
+++ biciamiga.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-05-2018 a las 02:46:27
+-- Tiempo de generación: 12-05-2018 a las 21:04:29
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.0.27
 
@@ -25,17 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `bicicletas`
+-- Estructura de tabla para la tabla `bike`
 --
 
-CREATE TABLE `bicicletas` (
-  `id_bici` int(11) NOT NULL,
-  `nombre` varchar(20) NOT NULL,
-  `descripcion` varchar(300) NOT NULL,
-  `foto` varchar(100) NOT NULL,
-  `precio_hr` decimal(10,0) NOT NULL,
-  `precio_dia` decimal(10,0) NOT NULL,
-  `disponibilidad` tinyint(1) NOT NULL
+CREATE TABLE `bike` (
+  `id` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `IdBikeType` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `biketype`
+--
+
+CREATE TABLE `biketype` (
+  `id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `image1` varchar(255) NOT NULL,
+  `image2` varchar(255) NOT NULL,
+  `image3` varchar(255) NOT NULL,
+  `image4` varchar(255) NOT NULL,
+  `image5` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,39 +58,22 @@ CREATE TABLE `bicicletas` (
 --
 
 CREATE TABLE `booking` (
-  `numberBooking` int(11) NOT NULL,
-  `user` varchar(500) NOT NULL,
+  `id` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
   `dateFrom` date NOT NULL,
   `dateTo` date NOT NULL,
-  `typeBike` int(11) NOT NULL,
-  `state` int(11) NOT NULL
+  `idTypeBike` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `totalPrice` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `booking`
---
-
-INSERT INTO `booking` (`numberBooking`, `user`, `dateFrom`, `dateTo`, `typeBike`, `state`) VALUES
-(1, 'admin', '2018-05-08', '0000-00-00', 1, 1),
-(4, '123', '2018-05-19', '2018-05-11', 1, 1),
-(5, '123', '2018-05-04', '2018-05-10', 2, 1),
-(6, '123', '2018-05-19', '2018-05-11', 0, 1),
-(7, '123', '2018-05-04', '2018-05-10', 0, 1),
-(8, '123', '2018-05-04', '2018-05-10', 2, 1),
-(9, '123', '2018-05-19', '2018-05-11', 2, 1),
-(10, '123', '2018-05-19', '2018-05-11', 2, 1),
-(11, '123', '2018-05-19', '2018-05-11', 2, 1),
-(12, '123', '2018-05-19', '2018-05-11', 2, 1),
-(13, '123', '2018-05-19', '2018-05-12', 2, 1),
-(15, '123', '2018-05-19', '2018-05-12', 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `user`
 --
 
-CREATE TABLE `usuario` (
+CREATE TABLE `user` (
   `dni` int(12) NOT NULL,
   `password` varchar(32) NOT NULL,
   `type` int(1) NOT NULL,
@@ -87,48 +83,39 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `usuario`
+-- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `usuario` (`dni`, `password`, `type`, `fullName`, `email`, `phone`) VALUES
-(12, 'f8e0920f299', 0, 'asdas', '123', 'asd'),
-(42, 'f8e0920f29985ad1a2724161e86faa65', 0, 'e234234234', 'werwer', 'werwerwer'),
-(123, '21232f297a57a5a743894a0e4a801fc3', 0, 'admin', 'admin@admin.com', '421196'),
-(999, 'f5bb0c8de146c67b44babbf4e6584cc0', 0, 'martin', 'martin@martin.com', '1239'),
-(1235, '827ccb0eea8', 0, 'facu', 'facu@facu.com', '12312'),
-(3234, '108a51ab7848e734dda43115cf3e7d5c', 0, 'MartÃ­n', 'martin@martin.com', 'kaka1231'),
-(8888, 'b6d767d2f8ed5d21a44b0e5886680cb9', 0, 'martin', 'admin@admin.com', '429912'),
-(33333, '61b80f94cdd', 0, 'fggdfdgd', '234', 'q34234234'),
-(123123, 'holis', 0, 'guille', 'asdlhasjd', '823478924'),
-(37450910, '827ccb0eea8a706c4c34a16891f84e7b', 0, 'Facundo Alvarez', 'dgas@aa.com', 'adsgasdgadsadsga'),
-(39953038, 'c4ca4238a0b923820dcc509a6f75849b', 0, 'martin', 'tinchin77@tinchin.com', '421196'),
-(423424234, 'md5(rwererw', 0, 'wrewerer', 'werwerwer', 'wrewerwer');
+INSERT INTO `user` (`dni`, `password`, `type`, `fullName`, `email`, `phone`) VALUES
+(123, '202cb962ac59075b964b07152d234b70', 1, 'Administrador', 'admin@admin.com.ar', '123456789');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `bicicletas`
+-- Indices de la tabla `bike`
 --
-ALTER TABLE `bicicletas`
-  ADD PRIMARY KEY (`id_bici`),
-  ADD UNIQUE KEY `id_bici` (`id_bici`),
-  ADD UNIQUE KEY `id_bici_3` (`id_bici`),
-  ADD KEY `id_bici_2` (`id_bici`),
-  ADD KEY `id_bici_4` (`id_bici`),
-  ADD KEY `id_bici_5` (`id_bici`);
+ALTER TABLE `bike`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_bike_bikeType` (`IdBikeType`);
+
+--
+-- Indices de la tabla `biketype`
+--
+ALTER TABLE `biketype`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`numberBooking`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `usuario`
+-- Indices de la tabla `user`
 --
-ALTER TABLE `usuario`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`dni`);
 
 --
@@ -136,16 +123,32 @@ ALTER TABLE `usuario`
 --
 
 --
--- AUTO_INCREMENT de la tabla `bicicletas`
+-- AUTO_INCREMENT de la tabla `bike`
 --
-ALTER TABLE `bicicletas`
-  MODIFY `id_bici` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `bike`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `biketype`
+--
+ALTER TABLE `biketype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `numberBooking` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `bike`
+--
+ALTER TABLE `bike`
+  ADD CONSTRAINT `fk_bike_bikeType` FOREIGN KEY (`IdBikeType`) REFERENCES `biketype` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
