4fae65c1dd4c2b64518c021867e99873f55d3d27
guillermina <guilleves@gmail.com>
2018-05-12 21:55:07 -0300

    working form to fetch and fill changes - wip actual push

--- modifyBikeType.php
+++ modifyBikeType.php
@@ -5,159 +5,152 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="bootstrap.css" media="screen">
     <link rel="stylesheet" href="bootswatch.min.css">
-  </head>
-		<body>
-
-              <?php
-              session_start();
-              if (isset($_COOKIE['recordar'])){
-                $_SESSION['fullName']=$_COOKIE['recordar'];
-              }
-              if(isset($_SESSION['fullName']))
-                $_fullName = (string)$_SESSION['fullName'];
-              ?>
-
-              <?php
-
-
-      //Save the values of the post.
-
-        if (isset($_POST['submit'])) {
-          //$user = $_SESSION[$_fullName];
-        //  $numberBooking = $_POST['numberBooking'];
-          $dateFrom = $_POST['dateFrom'];
-          $dateTo = $_POST['dateTo'];
-          $typeBike = $_POST['typeBike'];
-          $state = 1;
-
-
-          //validate the fields
-          if (empty($state) || empty($dateFrom) || empty($dateTo) || empty($typeBike))
-            $_errorValidacion = 1;
-          else {
-            if(isset($_GET['numberBooking'])) {
-              include ("connection.inc");
-
-                 $query = "UPDATE booking SET dateFrom='" . $dateFrom . "', dateTo='" . $dateTo . "', idTypeBike=" . $typeBike . ", status=" . $state. " where id=" . $_GET['numberBooking'];
-
-                mysqli_query($link, $query) or die (mysqli_error($link));
-                $_errorValidacion = 0;
-              }
-              else {
-                include ("connection.inc");
-                $query = "INSERT INTO booking (idUser, dateFrom, dateTo, idTypeBike, status) VALUES ('$_dni', '$dateFrom', '$dateTo', '$typeBike',1);";
-                mysqli_query($link, $query) or die (mysqli_error($link));
-                $_errorValidacion = 0;
-              }
-              mysqli_close($link);
-              header("location:showBooking.php");
-
-           }
-        }
-
-        ?>
-
-        <?php
-  if (isset($_GET['numberBooking'])) {
-    include ("connection.inc");
-    $numberBooking = $_GET['numberBooking'];
-    if ($resultado = $link->query('select * from booking where id =' . $numberBooking )) {
-      $fila = $resultado->fetch_assoc();
-
-       $dateFrom = $fila['dateFrom'];
-       $dateTo = $fila['dateTo'];
-       $typeBike = $fila['idTypeBike'];
-       $state = $fila['status'];
-
-    $modifica=1;
-}}
-  ?>
-
-  <div id="wrap">
-			<?php include("navBar.php") ?>
-<br>
-<br>
- <div class="container-fluid ">
-   <div class= "col-lg-12 text-center"><h3> <?php if (isset($modifica)) {
-                                if ($modifica == 1)
-                                  echo 'Modificar Reserva';
-                                  }
-                                  else
-                                    echo 'Nueva Reserva';
-                               ?></h3></div>
-  <div class = "col-lg-3"></div>
-  <div class = "col-lg-6">
-    <form name="submit" method="POST">
-
-            <label class="control-label">Número de Reserva</label>
-                    <input type="text" class="form-control" id="numberBooking" name="numberBooking" readonly="readonly" <?php if (isset($modifica)) { if ($modifica == 1) echo ' value="' . $numberBooking . '"';} ?>>
-
-                    <label  class="control-label">Fecha Desde</label>
-                    <input type="date" class="form-control" id="dateFrom" name="dateFrom" <?php if (isset($modifica)) {if ($modifica == 1) echo 'value="' . $dateFrom . '"';} ?>>
-
-                    <label  class="control-label">Fecha Hasta</label>
-                    <input type="date" class="form-control" id="dateTo" name="dateTo" <?php if (isset($modifica)) {if ($modifica == 1) echo 'value="' . $dateTo . '"';} ?>>
-
-                    <label  class="control-label">Tipo de Bicicleta</label>
-                    <div class="custom-select" >
-                        <select id="typeBike" class="selection" name="typeBike">
-                          <option  <?php if (isset($modifica)) {if ($typeBike == 0) echo 'selected="selected"' ;}?> value="0">Playera</option>
-                          <option  <?php if (isset($modifica)) {if ($typeBike == 1) echo 'selected="selected"' ;}?> value="1">Doble</option>
-                          <option <?php if (isset($modifica)) {if ($typeBike == 2) echo 'selected="selected"' ;}?>  value="2">MountainBike</option>
-                        </select>
-                      </div>
-
-                    <label  class="control-label">Estado</label>
-                      <div class="custom-select" >
-                        <select id="state" class="selection" name="state"  readonly="readonly">
-                          <option  <?php if (isset($modifica)) {if ($state == 1) echo 'selected="selected" ';}?> value="1"> Activa</option>
-                           <option  <?php if (isset($modifica)) {if ($state == 0) echo 'selected="selected"';}?> value="0"> Inactiva</option>
-                         </select>
-                      </div>
+</head>
+<body>
 
 <?php
-                     if (isset($_errorValidacion))
-                    {
-                       if ($_errorValidacion == 1)
-                       echo '<h4 class="alert alert-danger text-center">Ingrese todos los campos</h1>';
-                       if ($_errorValidacion == 2)
-                       echo '<h4 class="alert alert-danger text-center">El Número de Reserva ingresado no es valido</h1>';
-                       if ($_errorValidacion == 0)
-                       echo '<h4 class="alert alert-success text-center">La reserva se ah almacenado correctamente</h4>';
-                    }
-                     ?>
- <button type="reset" class="btn btn-warning col-lg-4 col-xs-5">Resetear</button>
+    session_start();
+    if(isset($_SESSION['fullName'])){
+        $_fullName = (string)$_SESSION['fullName'];
+        $_type = (int)$_SESSION['type'];
+    } else {
+        $_errorValidacion = 1;
+    }
+?>
 
-                          <button type="submit" name="submit" class="btn btn-primary col-lg-6 col-xs-6 pull-right">Agregar</button>
+<?php
+    if ($_type == 1) {
+        if (isset($_POST['submit'])) {
+
+            $id = $_POST['id'];
+            $description = $_POST['description'];
+            $price = $_POST['price'];
+            $image1 = $_POST['image1'];
+            $image2 = $_POST['image2'];
+            $image3 = $_POST['image3'];
+            $image4 = $_POST['image4'];
+            $image5 = $_POST['image5'];
+
+            if (empty($price)) {
+                $_errorValidacion = 3;
+            } else {
+                include ("connection.inc");
+
+                $query = "UPDATE bikeType SET description='" . $description . "', price='" . $price . "', image1=" . $image1 . ", image2='".$image2."', image3='".$image3."', image4='".$image4."', image5='".$image5."', status=" . $state. " where id=" . $_GET['id'];
+
+                mysqli_query($link, $query) or die (mysqli_error($link));
+                $_errorValidacion = 0;
+
+                mysqli_close($link);
+                header("location:showBikeTypes.php");
+            }
+        } else {
+            if(isset($_GET['id'])) {
+                echo $_GET['id'];
+                include ("connection.inc");
+
+                $query = "select * from bikeType where id = ".$_GET["id"];
+
+                $vResultado = mysqli_query($link, $query) or die (mysqli_error($link));;
+                $fila = mysqli_fetch_array($vResultado);
+                if(mysqli_num_rows($vResultado) == 0) {
+                    $_errorAutenticacion = 4;
+                } else {
+                    $id = $fila['id'];
+                    $description = $fila['description'];
+                    $price = $fila['price'];
+                    $image1 = $fila['image1'];
+                    $image2 = $fila['image2'];
+                    $image3 = $fila['image3'];
+                    $image4 = $fila['image4'];
+                    $image5 = $fila['image5'];
+
+                }
+                mysqli_close($link);
+            }
+        }
+
+    } else {
+        // must be admin to access
+        $_errorValidacion = 2;
+    }
+?>
+        <div id="wrap">
+            <?php include("navBar.php") ?>
+            <br>
+            <div class="container-fluid ">
+                <div class= "col-lg-12 text-center">
+                    <h3>Editar tipo de bicicleta</h3>
+                </div>
+                <div class = "col-lg-3"></div>
+                <div class = "col-lg-6">
+                    <form action="modifyBikeType.php" method="POST">
+                        <div class="form-group row">
+                            <div id="mensajes">
+                                <?php
+                                if (isset($_errorValidacion))
+                                {
+                                    if ($_errorValidacion == 2)
+                                    echo '<h4 class="alert alert-danger text-center">No está autorizado para realizar esta acción.</h1>';
+                                    if ($_errorValidacion == 1)
+                                    '<script type="text/javascript"> window.location = "/startSession.php" </script>';
+                                    if ($_errorValidacion == 3)
+                                    echo '<h4 class="alert alert-success text-center">El precio no puede estar vacío.</h4>';
+                                    if ($_errorValidacion == 4)
+                                    echo '<h4 class="alert alert-success text-center">No se ha encontrado ese tipo de bicicleta.</h4>';
+                                }
+                                ?>
+                            </div>
+                            <label class="control-label">Descripción</label>
+                            <input type="text" class="form-control" id="description" name="description" required value="<?php if(isset($_GET['id'])) {echo (string)$description ;} else {echo '';}?>" >
+                            <label class="control-label">Precio</label>
+                            <input type="number" max="999999999999" min="1" class="form-control" id="price" name="price" value="<?php if(isset($_GET['id'])) {echo (string)$price ;} else {echo '';}?>"  required>
+                        </div>
+                        <div class="form-group row" style="margin-top: 0;">
+                            <button type="reset" class="btn btn-warning col-lg-5 col-md-5 col-xs-12 pull" onclick="goBack();">Descartar cambios</button>
+                            <button type="submit" name="submit" id="submit" class="btn btn-primary col-lg-5 col-md-5 col-xs-12 pull-right">Actualizar</button>
+                        </div>
+                        <div class="clearfix"></div>
+                    </form>
+                </div>
+                <div class="col-lg-3"></div>
+            </div>
+        </div>
+
+
 
 
     </form> <!-- End Form -->
- <div class="col-lg-3"></div>
+    <div class="col-lg-3"></div>
 
-  </div> <!-- Close div to form -->
+</div> <!-- Close div to form -->
 
-  </div> <!-- End Container Fluid -->
- </div> <!-- End id Wrap -->
+</div> <!-- End Container Fluid -->
+</div> <!-- End id Wrap -->
 
 <?php include("footer.php") ?>
 
-		    <script src="jquery-1.10.2.min.js"></script>
-		    <script src="bootstrap.min.js"></script>
-		    <script src="bootswatch.js"></script>
+<script src="jquery-1.10.2.min.js"></script>
+<script src="bootstrap.min.js"></script>
+<script src="bootswatch.js"></script>
 
+<script type="text/javascript">
+    function goBack() {
+        window.location = "./showBikeTypes.php"
+    }
+</script>
+<!-- <script>
 
-       <!-- <script>
+function validaCampos(){
 
-        function validaCampos(){
+if ((document.getElementById("calle").value.length == 0) || (document.getElementById("numero").value.length == 0)){
+document.getElementById("mensajes").innerHTML = '<h4 class="alert alert-danger text-center">Complete todos los campos</h1>';
+return false;}
+else {
+return true;
+}
 
-          if ((document.getElementById("calle").value.length == 0) || (document.getElementById("numero").value.length == 0)){
-              document.getElementById("mensajes").innerHTML = '<h4 class="alert alert-danger text-center">Complete todos los campos</h1>';
-              return false;}
-          else {
-              return true;
-          }
-
-        }
-        </script>-->
-		</body>
-	</html>
+}
+</script>-->
+</body>
+</html>
